368d49d20929b2d0613c7f8232d5c9a69d1d9bf0
ishanrana363 <ishanrana363@gmail.com>
2025-02-03 18:11:07 +0600

    navbar design completed

---
+++ my-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='bg-green-400 py-3 ' >
+            <div className=' w-11/12 mx-auto  ' >
+                <nav>
+                    <ul className='flex justify-center gap-4 font-semibold  text-white  ' >
+                        <li> <Link href={"/"} >Home</Link>  </li>
+                        <li> <Link href={"/about"} >About Us</Link>  </li>
+                        <li> <Link href={"/faq"} >Faq</Link>  </li>
+                        <li> <Link href={"/contact"} >Contact</Link>  </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
